d4859bd0e627d66a17012a4cbac755ade0906730
Gribesh <gdhakal@apexrnd.be>
2020-10-04 18:30:55 +0545

    added routing and created simple page for calendar,cinema,restaurant

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,16 @@ router.get('/weekend', function(req, res, next) {
   res.render('index', { title: 'Development of Web Scraper' });
 });
 
+router.get('/calendar', function(req, res, next) {
+  res.render('calendar', { title: 'Calendar Page'});
+});
+
+router.get('/cinema', function(req, res, next) {
+  res.render('cinema', { title: 'Cinema Page'});
+});
+
+router.get('/restaurant', function(req, res, next) {
+  res.render('restaurant', { title: 'Restaurant Page'});
+});
+
 module.exports = router;
