ae152a9efbf08374aa43bc59e07243222908d601
Gribesh <gdhakal@apexrnd.be>
2020-10-04 19:17:22 +0545

    created friends file and added in calendar, also code beautified

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/data.json');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.redirect('/weekend');
 });
 
-router.get('/weekend', function(req, res, next) {
-  res.render('index', { title: 'Development of Web Scraper' });
+router.get('/weekend', function (req, res, next) {
+  res.render('index', {
+    title: 'Development of Web Scraper'
+  });
 });
 
-router.get('/calendar', function(req, res, next) {
-  res.render('calendar', { title: 'Calendar Page'});
+router.get('/calendar/:id', function (req, res, next) {
+  var name = req.params.id;
+  if (name) {
+    res.render('polo', {
+      title: name
+    });
+  } else {
+    res.render('calendar', {
+      title: 'Calendar Page',
+      friendsName: data['friendsName']
+    });
+  }
 });
 
-router.get('/cinema', function(req, res, next) {
-  res.render('cinema', { title: 'Cinema Page'});
+router.get('/cinema', function (req, res, next) {
+  res.render('cinema', {
+    title: 'Cinema Page'
+  });
 });
 
-router.get('/restaurant', function(req, res, next) {
-  res.render('restaurant', { title: 'Restaurant Page'});
+router.get('/restaurant', function (req, res, next) {
+  res.render('restaurant', {
+    title: 'Restaurant Page'
+  });
+});
+
+router.get('/marco/:id', function (req, res, next) {
+  var id = req.params.id;
+  res.render('polo', {
+    title: id
+  });
 });
 
 module.exports = router;
